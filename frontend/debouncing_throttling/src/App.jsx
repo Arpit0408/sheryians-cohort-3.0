6c3cb32e46facab0df3965b862bbf62f16786f54
Arpit0408 <paliwalarpit180@gmail.com>
2026-09-24 21:24:37 +0530

    chnage in debounce

--- frontend/debouncing_throttling/src/App.jsx
+++ frontend/debouncing_throttling/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-950 text-white">
-      <h1 className="text-3xl font-bold">App</h1>
+      <h1 className="text-3xl font-bold">Debounce</h1>
     </div>
   );
 };
